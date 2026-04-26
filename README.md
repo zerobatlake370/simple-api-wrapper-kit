9f6141de133a1a0ac427ade2d188e59db4b73327
Skyler Lee <shelbageiszler3358871@gmail.com>
2026-04-26 08:30:21 +0200

    Initial commit: api wrapper

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# simple-api-wrapper-kit
-A simple api wrapper project
+# Api Wrapper
+
+A simple api wrapper project.
+\n
+## Getting Started
+
+```bash
+python app.py
+```
---
+++ app.py
@@ -0,0 +1,10 @@
+"""
+Api Wrapper - Main module.
+"""
+
+def main():
+    print("Api Wrapper is running...")
+    print("Version 0.1.0")
+
+if __name__ == "__main__":
+    main()
